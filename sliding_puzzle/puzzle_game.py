0d30475a9a956f7b72750c2ecc10977614478d5d
eastmountain <59405316+eastmountaincode@users.noreply.github.com>
2021-12-29 12:01:49 -0500

    Add files via upload

---
+++ sliding_puzzle/puzzle_game.py
@@ -0,0 +1,17 @@
+'''
+    Andrew Boylan
+    Fall 2021
+
+    Main driver file.
+    
+'''
+import CodeResources.setup as setup
+import turtle
+
+def main():
+    # RUN THE SETUP
+    setup.run_setup()
+    turtle.mainloop()
+
+if __name__ == "__main__":
+    main()
